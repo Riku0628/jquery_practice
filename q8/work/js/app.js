11a51ca29bfd046dccce884d1f5ebd662680e8c8
Riku628 <lludou29@gmail.com>
2021-03-22 02:27:02 +0900

    修正２

--- q8/work/js/app.js
+++ q8/work/js/app.js
@@ -1,18 +1,19 @@
 $(function() {
-  const messNone = $("<div class='message'>" + "検索結果が見つかりませんでした。" + "<br>" + "別のキーワードで検索して下さい。" + "</div>");
-  errMes = $('<div class="message">' + "正常に通信できませんでした。" + '<br>' + "インターネットの接続の確認をしてください" + '</div>');
-  let air = "";
-  pageCount = 1
-
+  let last_search = "";
+  let pageCount = 1
 
   $('.search-btn').on('click',function() {
     //.search-btnがクリックされたとき
     let searchWord = $('#search-input').val();
     //searchWordにsearch-input内容を取得する
-    searchWord !== air ? (pageCount=1,$(".lists").empty(), $(".message").remove() ) : pageCount++;
-    //searchWordとairの内容を比較して違う場合pageCountを1にしてlistsを消去して、messageも消去する。同じ場合pageCountを+1にする
-    air = searchWord
-    //airにsearchWorldの内容を格納する
+    if (searchWord !== last_search){
+      pageCount = 1,$(".lists").empty(), $(".message").remove();
+      }else{
+        pageCount++;
+      };
+    //searchWordとlast_searchの内容を比較して違う場合pageCountを1にしてlistsを消去して、messageも消去する。同じ場合pageCountを+1にする
+    last_search = searchWord
+    //last_searchにsearchWorldの内容を格納する
     const settings = {
       "url": `https://ci.nii.ac.jp/books/opensearch/search?title=${searchWord}&format=json&p=${pageCount}&count=20`,
       "method": "GET",}
@@ -24,13 +25,17 @@ $(function() {
       });
 
 
+
     function displayResult(result) {
-      result[0].items == void 0 ? $('.inner').prepend(messNone) :
-      //result[0].itemsの中が0ではないか比較する。0ならばmessNoneを表示させる。0でないならeachの処理に移る。
+      const messNone = $("<div class='message'>" + "検索結果が見つかりませんでした。" + "<br>" + "別のキーワードで検索して下さい。" + "</div>");
+      if (result[0].items === undefined) {
+        $('.inner').prepend(messNone)
+      } else {
+      //result[0].itemsの中がundefinedではないか比較する。undefinedならばmessNoneを表示させる。undefinedでないならeachの処理に移る。
       $.each(result[0].items, function(index, library) {
         //result[0].itemsに含まれている要素をループ処理させる。
         const dsp = $("<li class='lists-item'>" + "<div class='list-inner'>" +
-        "<p>タイトル：" + (library.title ? library.title : "タイトル不明" )  + "</p>" +
+        "<p>タイトル：" +  (library.title ? library.title : "タイトル不明" ) + "</p>" +
         //library.titleとundefinedを比較する。一致しなかった場合library.titleを、一致した場合「タイトル不明」をそれぞれ表示する。
         "<p>作者：" + (library['dc:creator'] ? library['dc:creator'] : "作者不明" ) + "</p>" +
         //library['dc:creator']とundefinedを比較する。一致しなかった場合library['dc:creator']を、一致した場合「作者不明」をそれぞれ表示する。
@@ -42,13 +47,17 @@ $(function() {
         $(".lists").prepend(dsp) ; //dspを表示する
       });
     };
+  };
 
-    function displayError(err) {　//通信が失敗したときの処理
+    function displayError() {　//通信が失敗したときの処理
+      const errMes = $('<div class="message">' + "正常に通信できませんでした。" + '<br>' + "インターネットの接続の確認をしてください" + '</div>');
       $(".lists").empty();　//listsの中を消去する
       $(".message").remove(); //messageを消去する
-      $('.inner').prepend(errMes); //eroMesを表示する
-    }
-  });
+      $('.inner').prepend(errMes); //errMesを表示する
+      console.log("ajax通信に失敗しました。");
+    };
+
+
 
   $(".reset-btn").on("click", function () { //.reset-btnをクリックしたときの処理
     pageCount = 1;　//ページカウントを１に戻す
@@ -58,7 +67,7 @@ $(function() {
   });
 });
 
-
+});
 
 
 
