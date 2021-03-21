d80c2e22da0d4cb2ac34e7a257be08d5934421eb
Riku628 <lludou29@gmail.com>
2021-03-22 02:35:34 +0900

    修正２

--- q6/work/js/app.js
+++ q6/work/js/app.js
@@ -1,14 +1,17 @@
 $(function() {
   const selectContent = $(".select-box");
-  selectContent.on("change", function() {
+  selectContent.on("change", function() { //select-boxの内容が変わったときに
     let selectcategory = $(selectContent).val()
-    "all" === selectcategory ? $(".food-list li").show() :
-    $('.food-list li').each(function() {
-      if ($(this).data('category-type') === selectcategory) {
-        $(this).show();
+    if ( "all" === selectcategory ){  //allとselectcategoryを比較して一致すれば.food-list liを表示する
+      $(".food-list li").show();
+    } else {  //一致しなければelseの処理にうつる
+    $('.food-list li').each(function() { //food-listをeachでループ処理する
+      if ($(this).data('category-type') === selectcategory) { //food-list liのcategory-typeとselectcategoryを比較する
+        $(this).show(); //一致すれば表示する
       } else {
-        $(this).hide();
+        $(this).hide(); //一致しなければ表示しない
     }
   })
+  }
   })
  });
